6572a3ec8a831e09abb6c29872398bb265732624
Josephy Cruz Araújo <josephy.araujo@escolar.ifrn.edu.br>
2025-11-22 11:05:37 -0300

    :bug: fix: update url

--- BancoApiGateway/api-gateway/src/banco-core-soap/banco-core-soap.service.ts
+++ BancoApiGateway/api-gateway/src/banco-core-soap/banco-core-soap.service.ts
@@ -3,7 +3,7 @@ import { createClientAsync } from 'strong-soap';
 
 @Injectable()
 export class BancoCoreSoapService {
-  private wsdlUrl = 'http://localhost:8080/wsdl';
+  private wsdlUrl = 'http://localhost:8080/ws/banco.wsdl';
 
   async chamarServico(acao: string, payload: any): Promise<any> {
     const client = await createClientAsync(this.wsdlUrl);
