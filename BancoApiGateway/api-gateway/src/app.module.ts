62dd1c04ea9471c0cdfff82d37d747408cba7047
DanielBR0612 <danielfranciscobragadearaujo@gmail.com>
2026-01-20 09:49:22 -0300

    feat: monitoramento de atm

--- BancoApiGateway/api-gateway/src/app.module.ts
+++ BancoApiGateway/api-gateway/src/app.module.ts
@@ -5,15 +5,17 @@ import { BancoCoreSoapModule } from './banco-core-soap/banco-core-soap.module';
 import { BancoRestApiModule } from './banco-rest-api/banco-rest-api.module';
 import { BancoSoapController } from './banco/banco-soap.controller';
 import { BancoRestController } from './banco/banco-rest.controller';
-
 import { AuthModule } from './auth/auth.module';
 import { AuthController } from './auth/auth.controller';
 import { NotificationsModule } from './notifications/notifications.module';
 import { QueueService } from './queue/queue.service';
+import { AtmService } from './atm/atm.service';
+import { AtmController } from './atm/atm.controller';
+import { AtmModule } from './atm/atm.module';
 
 @Module({
-  imports: [BancoCoreSoapModule, BancoRestApiModule, NotificationsModule, AuthModule],
-  controllers: [AppController, BancoSoapController, BancoRestController,  AuthController],
-  providers: [AppService, QueueService],
+  imports: [BancoCoreSoapModule, BancoRestApiModule, NotificationsModule, AuthModule, AtmModule],
+  controllers: [AppController, BancoSoapController, BancoRestController,  AuthController, AtmController],
+  providers: [AppService, QueueService, AtmService],
 })
 export class AppModule {}
